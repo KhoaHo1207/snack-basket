1654e1111d970c22dcd5cb2e5697e3c59ef6f656
Thomas Ho <khoaho120703@gmail.com>
2026-01-06 16:10:54 +0700

    [server] feat(auth api): resend otp, logout, refreshtoken, forgot password, reset password, getCurrentUser api

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -233,4 +233,226 @@ const resendOTP = async (req, res) => {
     });
   }
 };
-module.exports = { login, register, verifyOTP, resendOTP };
+
+const logout = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    const user = await User.findOne({ refreshToken });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    user.refreshToken = undefined;
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Logout successful",
+    });
+  } catch (error) {
+    console.error("Logout error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const refreshToken = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token is required",
+      });
+    }
+    const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid refresh token",
+      });
+    }
+    const user = await User.findById(decoded._id);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const accessToken = generateAccessToken(user._id);
+    return res.status(200).json({
+      success: true,
+      message: "Refresh token successful",
+      data: {
+        accessToken,
+      },
+    });
+  } catch (error) {
+    console.log("Refresh token failed", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is required",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const { otp, hashedOTP } = await generateOTP();
+    user.otp = hashedOTP;
+    user.otpExpire = new Date(Date.now() + 10 * 60 * 1000);
+    await user.save();
+    await sendEmail({
+      to: email,
+      subject: "Forgot password",
+      html: `
+        <h2>Forgot password</h2>
+        <p>Your OTP code:</p>
+        <h1>${otp}</h1>
+        <p>Expires in 10 minutes</p>
+      `,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "OTP sent to email",
+    });
+  } catch (error) {
+    console.log("Forgot password failed", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const { email, otp, password } = req.body;
+    if (!email) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is required",
+      });
+    }
+    if (!otp) {
+      return res.status(400).json({
+        success: false,
+        message: "OTP is required",
+      });
+    }
+    if (!password) {
+      return res.status(400).json({
+        success: false,
+        message: "Password is required",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    if (user.otpExpire < Date.now()) {
+      return res.status(400).json({
+        success: false,
+        message: "OTP expired",
+      });
+    }
+    const isCorrectOTP = await bcrypt.compare(otp, user.otp);
+    if (!isCorrectOTP) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP",
+      });
+    }
+    user.password = await bcrypt.hash(password, 10);
+    user.otp = undefined;
+    user.otpExpire = undefined;
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "Password reset successfully",
+    });
+  } catch (error) {
+    console.log("Reset password failed", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const getCurrentUser = async (req, res) => {
+  try {
+    const id = req.user._id;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "User found",
+      data: user,
+    });
+  } catch (error) {
+    console.log("Get current user failed", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = {
+  login,
+  register,
+  verifyOTP,
+  resendOTP,
+  logout,
+  refreshToken,
+  forgotPassword,
+  resetPassword,
+  getCurrentUser,
+};
+
+// register
+// verify
+// resend-otp
+// login
+// refresh-token
+// logout
+// forgot-password
+// reset-password
+// me
